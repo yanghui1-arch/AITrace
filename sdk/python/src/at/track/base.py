991d752d9d41cf411cdc9c162f942f3e5d8044fe
dass90 <3053034939@qq.com>
2025-09-25 14:15:48 +0800

    feat(BaseTracker): basic simpliest Tracker class

--- sdk/python/src/at/track/base.py
+++ sdk/python/src/at/track/base.py
@@ -1,4 +1,6 @@
+from typing import Callable, Any, Tuple, Dict
 from abc import ABC, abstractmethod
+import functools
 
 class TrackerOptions:
     ...
@@ -7,23 +9,87 @@ class BaseTracker(ABC):
     """ Base tracker to track all output
     Every tracker should be extended `BaseTracker` class.
     Following methods need to be implemented in subclass.
-        * 
-        *
-        *
 
     Args:
-
+        provider(Optional[str]): provider name
     """
 
-    def __init__(self, options: TrackerOptions | None = None):
+    def __init__(self):
+        self.provider: str | None = None
+
+    def track(
+        self,
+        func:Callable,
+        project_name: str | None = None
+    ) -> Callable:
+        """ track decorator
+
+        Args:
+            project_name(Optional[str]): project name to be tracked. Default to None
         
-        self.options = options
+        Returns:
+            Callable: a decorator
+        """
 
-    def track(self):
+        if project_name is None:
+            # post a request to get caller project list
+            # if caller has `default project` name project, create a project named like default project - 2 
+            project_name = "Default project"
+
+        return self._decorator(func=func)
+    
+    def _decorator(
+        self,
+        func:Callable,
+    ) -> Callable:
+        """ construct a decorator 
+        
+        Args:
+            func(Callable): a callable function
+        
+        Returns:
+            Callable: track decorator
+        """
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            result = None
+            func_exception: Exception | None = None
+            error_info: Exception | None = None
+            # before track
+            self._before_calling_function(
+                func=func,
+                args=args,
+                kwargs=kwargs
+            )
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                func_exception = e
+            finally:
+                # after track
+                self._after_calling_function(output=result, error_info=error_info)
+                if func_exception is not None:
+                    raise func_exception
+                else:
+                    return result
+
+        return wrapper
+
+    def _before_calling_function(
+        self,
+        func:Callable,
+        args:Tuple,
+        kwargs:Dict[str, Any]
+    ):
+        """ preparation for logging input before track function """
         ...
 
-    def __before_track(self):
-        ...
-
-    def __after_track(self):
+    def _after_calling_function(
+        self,
+        output:Any,
+        error_info:str
+    ):
+        """ preparation for logging output after track function """
         ...
