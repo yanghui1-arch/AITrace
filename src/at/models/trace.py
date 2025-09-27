44c51a96b26273ac4596b63084de0be12318b46d
dass90 <3053034939@qq.com>
2025-09-27 15:33:05 +0800

    feat(trace): add fields in trace class

--- src/at/models/trace.py
+++ src/at/models/trace.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+from datetime import datetime
 
 class Trace:
     """LLM Trace
@@ -32,6 +34,35 @@ class Trace:
     the new pair should be included in the trace when and only when the previous conversations except current pair are the same
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        tags:List[str] | None = None,
+        llm_input_output: List[Dict] | None = None,
+        provider: str | None = None,
+        consume_tokens: int | None = None,
+        last_updated_time:datetime = datetime.now()
+    ):
+        """Initialize trace
+        
+        Args:
+            tags(List[str] | None): trace tags. Default to `None`.
+            llm_input_output(List[Dict] | None): a list of dict that llm input and output. Default to `None`. 
+            provider(str | None): llm provider name. Default to `None`.
+            consume_tokens(int | None): consume tokens of the trace.
+            last_updated_time(datetime): latest update time of trace. Default to `current datetime in caller's zone.`
+        """
+        
+        if llm_input_output is None:
+            import warnings
+            warnings.warn("You create a trace that llm_input_output is None.")
+        
+        self.llm_input_output = llm_input_output
+        self._provider = provider
+        self.last_updated_time = last_updated_time
+        self.tags = tags
+        self.consume_tokens = consume_tokens
+
+    @property
+    def provider(self):
+        return self._provider
     
