0521afdb729e75e330ffe1da5c35c1fc65d56e09
dass90 <3053034939@qq.com>
2025-09-28 20:02:01 +0800

    Update

--- at/setup.py
+++ at/setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='at',
     version='0.1.0.beta',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
+        "click>=8.3.0",
         "openai>=1.108.2",
     ],
     entry_points={
