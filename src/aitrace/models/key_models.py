d6af97a76e232de264841ca39a46f967c2287bdb
dass90 <3053034939@qq.com>
2025-10-06 23:06:27 +0800

    feat(models): three key models

---
+++ src/aitrace/models/key_models.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from enum import Enum
+from datetime import datetime
+from typing import Any, Dict, List
+
+class StepType(Enum):
+    CUSTOMIZED = 'customized'
+    LLM_RESPONSE = 'llm_response'
+    RETRIEVE = 'retrieve'
+    TOOL = 'tool'
+
+@dataclass
+class Step:
+	project_name: str
+	name: str
+	id: str
+	trace_id: str
+	type: StepType = StepType.CUSTOMIZED
+	input: Dict[str, Any] | None = None
+	output: Any | None = None
+	error_info: str | None = None
+
+@dataclass
+class Track:
+    _step: Step
+    call_timestamp: datetime
+
+    @property
+    def step(self):
+        return self._step
+    
+    @property
+    def project_name(self):
+        return self._step.project_name
+    
+@dataclass
+class Trace:
+    project_name: str
+    id: str
+    conversation_id: str
+    name: str
+    model: str | None = None
+    input: Dict[str, Any]
+    output: Any | None = None
+    error_info: str | None = None
+
+@dataclass
+class Conversation:
+    project_name: str
+    id: str
+    name: str
+    traces: List[Trace]
+    start_time: datetime
+    last_update_time: datetime
