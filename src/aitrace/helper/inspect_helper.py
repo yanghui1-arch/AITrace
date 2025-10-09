f589970f6a0e224bba9de398a171893f9521fcd5
dass90 <3053034939@qq.com>
2025-10-09 20:39:58 +0800

    feat(inspect_helper): feat inspect helper parse args and kwargs to dict type input

--- src/aitrace/helper/inspect_helper.py
+++ src/aitrace/helper/inspect_helper.py
@@ -15,4 +15,33 @@ def parse_to_dict_input(
     Returns:
         Dict[str, Any]: input with dict type
     """
-    ...
+    
+    sig = inspect.signature(func)
+    
+    # Create binding of arguments to parameters
+    bound_args = sig.bind(*args, **kwargs)
+    bound_args.apply_defaults()
+    
+    return dict(bound_args.arguments)
+
+if __name__ == '__main__':
+    import functools
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(f"decorator {func.__name__}: {parse_to_dict_input(func, args, kwargs)}")
+            return result
+        return wrapper
+
+    @decorator
+    def init(test_init, k, ms, llm='', ticket={}, *args, **kwargs):
+        ...
+
+    init(1, 2, 3, llm='maga', ticket={"key": "value"})
+
+    @decorator
+    def post(test, b, quick, is_false=False):
+        ...
+
+    post(1, 2, 3, True)
