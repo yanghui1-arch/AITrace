18d8b543a884f99d2c7e83c3f43ca841d0d22a29
dass90 <3053034939@qq.com>
2025-10-21 22:32:48 +0800

    feat: create user

---
+++ backend/aitrace-java-backend/src/main/java/com/supertrace/aitrace/domain/User.java
@@ -0,0 +1,40 @@
+package com.supertrace.aitrace.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Email(message = "Wrong email format")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(name = "username")
+    private String username;
+
+    @NotBlank
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @Column(name = "register_time", nullable = false)
+    private LocalDateTime registerTime;
+
+    @PrePersist
+    protected void onCreate() {
+        this.registerTime = LocalDateTime.now();
+    }
+}
