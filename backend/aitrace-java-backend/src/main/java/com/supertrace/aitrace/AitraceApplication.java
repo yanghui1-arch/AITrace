e5f627ad7a0d0d3330d512d46a399f92ba1f109f
dass90 <3053034939@qq.com>
2025-10-21 19:55:49 +0800

    Init Java Backend

---
+++ backend/aitrace-java-backend/src/main/java/com/supertrace/aitrace/AitraceApplication.java
@@ -0,0 +1,13 @@
+package com.supertrace.aitrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitraceApplication.class, args);
+    }
+
+}
---
+++ backend/aitrace-java-backend/src/test/java/com/supertrace/aitrace/AitraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supertrace.aitrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
