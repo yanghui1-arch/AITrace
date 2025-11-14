38dc05e0529abcb6c2ab59302b65dba666d39c80
dass90 <3053034939@qq.com>
2025-11-14 11:11:14 +0800

    [WEB] feat: implement customized input and output nodes.

--- web/src/components/trace-dialog/trace-process-io-node.tsx
+++ web/src/components/trace-dialog/trace-process-io-node.tsx
@@ -1,38 +1,38 @@
 import { memo } from "react";
-import {
-  BaseNode,
-  BaseNodeHeader,
-  BaseNodeHeaderTitle,
-} from "@/components/xyflow-ui/base-node";
-import { Rocket } from "lucide-react";
 import { Handle, Position, type NodeProps } from "@xyflow/react";
+import { FunctionIOCard } from "../fn-io-card";
 
 export const TraceIONode = memo(({ data }: NodeProps) => {
-  const io = data.input ? data.input : data.output;
+  const io = (data.input ? data.input : data.output) as string | Record<string, unknown>;
   const source = data.input ? true : false;
+  const errorInfo = data.errorInfo as string | undefined;
 
   return (
     /* first node doesn't contain input handle and the last node doesn't contain output handle */
-    <BaseNode className="w-auto max-w-64">
-      {!source && <Handle
-        type="target"
-        position={Position.Top}
-        className="bg-blue-400! hover:bg-blue-500!"
-      />}
+    <div className="w-auto max-w-64">
+      {!source && (
+        <Handle
+          type="target"
+          position={Position.Top}
+          className="bg-blue-400! hover:bg-blue-500!"
+        />
+      )}
 
-      <BaseNodeHeader className="justify-center">
-        <Rocket className="size-4" />
-        <BaseNodeHeaderTitle className="wrap-break-words text-sm truncate">
-          {JSON.stringify(io)}
-        </BaseNodeHeaderTitle>
-      </BaseNodeHeader>
+      <FunctionIOCard
+        data={io}
+        labelTitle={source ? "Input" : "Output"}
+        errorInfo={errorInfo}
+        className="nowheel"
+      />
 
-      {source && <Handle
-        type="source"
-        position={Position.Bottom}
-        className="bg-blue-400! hover:bg-blue-500!"
-      />}
-    </BaseNode>
+      {source && (
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          className="bg-blue-400! hover:bg-blue-500!"
+        />
+      )}
+    </div>
   );
 });
 
