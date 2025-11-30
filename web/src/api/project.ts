c7246b4ab61e9b82f34c70042dd7d450142ea07c
dass90 <3053034939@qq.com>
2025-11-30 17:29:35 +0800

    [web] feat: get all projects information

--- web/src/api/project.ts
+++ web/src/api/project.ts
@@ -6,12 +6,20 @@ type RequestDataType = {
     projectDescription: string
 }
 
-type ReturnProjectNameType = string
+type ReturnCreateProjectNameType = string
+type Project = {
+    projectName: string
+    description: string
+    averageDuration: number
+    cost: number
+    createdTimestamp: string
+    lastUpdateTimestamp: string
+}
 
-type Response = {
+type Response<T> = {
     code: number,
     message: string,
-    data: ReturnProjectNameType
+    data: T
 }
 
 export const projectApi = {
@@ -19,9 +27,15 @@ export const projectApi = {
         projectName,
         projectDescription,
     }: RequestDataType) {
-        return http.post<Response>(
+        return http.post<Response<ReturnCreateProjectNameType>>(
             "/v0/project/create_new_project",
             {projectName, projectDescription},
+        )
+    },
+
+    getAllProjects() {
+        return http.get<Response<Project[]>> (
+            "/v0/project/get_all_projects"
         )
     }
 };
--- web/src/pages/projects/index.tsx
+++ web/src/pages/projects/index.tsx
@@ -1,235 +1,52 @@
 import { DataTable } from "@/components/data-table";
 import { projectColumns } from "./project-columns";
+import { useEffect, useState } from "react";
+import { projectApi } from "@/api/project";
 
+type Project = {
+  name: string;
+  description: string;
+  cost: number;
+  avgDuration: number;
+  lastUpdateTimestamp: string;
+};
 
 export default function ProjectsPage() {
-  const project = [{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "TASK-8782",
-    description: "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
-    cost: 1.2,
-    avgDuration: 23.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.01,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },{
-    name: "project",
-    description: "123",
-    avgDuration: 1.0,
-    cost: 1.2,
-    lastUpdateTimestamp: "2025-10-27",
-  },
-]
+  const [project, setProject] = useState<Project[]>([]);
+  useEffect(() => {
+    const getProjects = async () => {
+      const response = await projectApi.getAllProjects();
+      if (response.data.code == 200) {
+        const userProjects = response.data.data;
+        const projects: Project[] = userProjects.map((p) => ({
+          name: p.projectName,
+          description: p.description,
+          cost: p.cost,
+          avgDuration: p.averageDuration,
+          lastUpdateTimestamp: p.lastUpdateTimestamp,
+        }));
+        setProject(projects);
+      } else if (response.data.code == 404) {
+        console.warn("No projects found.")
+      }
+    };
+    getProjects();
+  }, []);
 
   return (
     <div className="px-4 lg:px-6">
       <h2 className="text-xl font-semibold">Projects</h2>
-      <p className="text-muted-foreground mt-2">Create a new one project to track and improve your agent performance!</p>
+      <p className="text-muted-foreground mt-2">
+        Create a new one project to track and improve your agent performance!
+      </p>
       <div className="container mx-auto py-5">
-        <DataTable data={project} columns={projectColumns} hasCreateProjectComponent={true} isNavigate={true}/>
+        <DataTable
+          data={project}
+          columns={projectColumns}
+          hasCreateProjectComponent={true}
+          isNavigate={true}
+        />
       </div>
     </div>
   );
 }
-
