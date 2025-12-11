33c7d3c62f3f36c4cf0add298164e605e8d2bc59
dass90 <3053034939@qq.com>
2025-12-11 09:44:12 +0800

    [web] fix: fix user avatar bright

--- web/src/components/nav/nav-user.tsx
+++ web/src/components/nav/nav-user.tsx
@@ -47,7 +47,7 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.userName} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
