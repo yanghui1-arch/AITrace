84f5be913587ee1b32bbf89fdfeb490a771e7061
dass90 <3053034939@qq.com>
2025-12-25 00:20:50 +0800

    [kubent] feat: select steps

--- kubent/src/repository/models/__init__.py
+++ kubent/src/repository/models/__init__.py
@@ -1,7 +1,9 @@
+from .step import Step
 from .kubent_chat_session import KubentChatSession
 from .kubent_chat import KubentChat
 
 __all__ = [
+    "Step",
     "KubentChatSession",
     "KubentChat",
 ]
