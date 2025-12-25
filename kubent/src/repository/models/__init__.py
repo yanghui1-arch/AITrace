5e8694e590ccedcc3fd3ce6120787f0355ba02dd
dass90 <3053034939@qq.com>
2025-12-25 11:42:58 +0800

    [kubent] feat: implement search traces

--- kubent/src/repository/models/__init__.py
+++ kubent/src/repository/models/__init__.py
@@ -1,9 +1,11 @@
 from .step import Step
+from .trace import Trace
 from .kubent_chat_session import KubentChatSession
 from .kubent_chat import KubentChat
 
 __all__ = [
     "Step",
+    "Trace",
     "KubentChatSession",
     "KubentChat",
 ]
